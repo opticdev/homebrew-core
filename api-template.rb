cb4ba03314bc63b6c6ef9d281f712ae9bd613eb0
lou <lou@useoptic.com>
2021-02-22 10:02:22 -0500

    init

---
+++ api-template.rb
@@ -0,0 +1,34 @@
+require "language/node"
+
+class Api < Formula
+  desc "Optic CLI"
+  homepage "https://github.com/opticdev/optic"
+  url "{URL}"
+  sha256 "{SHASUM}"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+
+    # Set shebang to Homebrew cellar
+    bin_location = "#{libexec}/bin/api"
+    lines = IO.readlines(bin_location)
+    lines[0] = "#!#{HOMEBREW_PREFIX}/opt/node/bin/node"
+
+    File.open(bin_location, "w") do |file|
+      file.puts lines
+    end
+
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "@useoptic/cli", shell_output("#{bin}/api --version | awk '{print $1}'")
+  end
+end
